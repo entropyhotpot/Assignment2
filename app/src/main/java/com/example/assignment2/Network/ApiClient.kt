986ca0beaf8279fb8782cb91656a92a030d35b0a
Rob <hansford@gmail.com>
2024-09-09 14:38:28 +1000

    Add initial api client class

---
+++ app/src/main/java/com/example/assignment2/Network/ApiClient.kt
@@ -0,0 +1,34 @@
+package com.example.assignment2.Network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+class ApiClient {
+
+    private val BASE_URL = "https://vu-nit3213-api.onrender.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(client)
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
+}
