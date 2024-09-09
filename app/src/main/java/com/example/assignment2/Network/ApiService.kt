4c15035e48ac91a0197a68355652aadc74a01a05
Rob <hansford@gmail.com>
2024-09-09 14:37:32 +1000

    Add initial api service interface

---
+++ app/src/main/java/com/example/assignment2/Network/ApiService.kt
@@ -0,0 +1,18 @@
+package com.example.assignment2.Network
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.POST
+
+interface ApiService {
+
+    // Request to login to API
+    @POST("footscray/auth")
+    suspend fun authAPI(@Body data: authRequest): authRequest
+
+    // Request to grab all items
+    @GET("dashboard/{topic}")
+    suspend fun getAllItems(@Path("topic") topic: String): List<ResponseItem>
+
+}
