03785e8d1c3420dfd0bb46b27ad9932decb7afbf
Rob <hansford@gmail.com>
2024-09-09 14:39:03 +1000

    Add additional dependencies required for Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,14 @@ dependencies {
     // Material components
     implementation("com.google.android.material:material:1.12.0")
 
+    // Retrofit/API components
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.11.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
