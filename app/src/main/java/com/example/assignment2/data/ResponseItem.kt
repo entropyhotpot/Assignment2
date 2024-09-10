37f1feadb1d557139233222dc442d990ab07348e
Rob <hansford@gmail.com>
2024-09-10 16:23:36 +1000

    Add parcebale dependencies and fix nav_graph arguments to correct fragment

--- app/src/main/java/com/example/assignment2/data/ResponseItem.kt
+++ app/src/main/java/com/example/assignment2/data/ResponseItem.kt
@@ -1,11 +1,14 @@
 package com.example.assignment2.data
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class ResponseItem(
     @Json(name = "destination") val destination: String,
     @Json(name = "country") val country: String,
     @Json(name = "bestSeason") val bestSeason: String,
     @Json(name = "popularAttraction") val popularAttraction: String,
     @Json(name = "description") val description: String
-)
+): Parcelable
